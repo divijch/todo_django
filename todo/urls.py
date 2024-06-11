7607e4acd15f52c994d64a72b58c797f6036c9e4
Divij Chaudhry <divijchaudhry@MacBook-Pro.local>
2024-06-11 15:03:31 +0530

    Full Project Update

--- todo/urls.py
+++ todo/urls.py
@@ -3,8 +3,20 @@ from . import views
 
 urlpatterns = [
     
-    
+    #Add Task Feature
     path('addtask/', views.addtask, name = 'addtask'),
+    
+    #Mark As Done Feature
     path('mark_as_done/<int:pk>', views.mark_as_done, name='mark_as_done'),
+    
+    #Mark As Undone Feature
+    path('mark_as_undone/<int:pk>', views.mark_as_undone, name  = 'mark_as_undone'),
+    
+    #Edit Feature
+    path('edit_task/<int:pk>', views.edit_task, name = 'edit_task'),
+    
+    #Delete Feature
+    path('delete_task/<int:pk>', views.delete_task, name = 'delete_task'),
+    
     
 ]
--- todo/views.py
+++ todo/views.py
@@ -12,3 +12,28 @@ def mark_as_done(request, pk):
     task.is_completed = True
     task.save()
     return redirect('home')
+
+def mark_as_undone(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = False
+    task.save()
+    return redirect('home')
+
+def edit_task(request, pk):
+    get_task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        new_task = request.POST['task']
+        get_task.task = new_task
+        get_task.save()
+        return redirect('home')
+    else:
+        context= {
+            'get_task' : get_task
+        }
+        
+        return render(request, 'edit_task.html', context)
+    
+def delete_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.delete()
+    return redirect('home')
